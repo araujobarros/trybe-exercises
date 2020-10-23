d87731c5a7ae45f0f95721f368405cf71d0a3fe4
Edson Araujo <araujo.barros@matematica.ufrj.br>
2020-10-23 19:31:58 -0300

    Bonus 3

--- bloco_4/dia_2/bonus.js
+++ bloco_4/dia_2/bonus.js
@@ -1,5 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+
+//Bonus - 3 - Agora você irá criar um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+
+let newNumbers = []
+for(let i = 0; i < numbers.length; i++){
+    if (i !== (numbers.length-1)){
+       newNumbers.push(numbers[i]*numbers[i+1]);
+    } else {
+        newNumbers.push(numbers[i]*2);
+    }
+}
+console.log(newNumbers);
+
 //Bonus - 1 - Ordene o array numbers em ordem crescente e imprima seus valores;
 
 for(let i = 1; i < numbers.length; i++){
@@ -27,3 +40,4 @@ for(let i = 1; i < numbers.length; i++){
 }
 
 console.log(numbers);
+
