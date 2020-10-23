572dccaf29b207de4e0aecad3543b7243aabc4e1
Edson Araujo <araujo.barros@matematica.ufrj.br>
2020-10-23 18:03:53 -0300

    Exercícios: 1 ao 5

--- bloco_4/dia_2/pratica.js
+++ bloco_4/dia_2/pratica.js
@@ -5,3 +5,41 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for(var i in numbers){
     console.log(numbers[i]);
 }
+
+// 2 - Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+let soma = 0;
+
+for (var i in numbers){
+    soma = soma + numbers[i];
+}
+console.log("A soma é: " + soma);
+
+/* 3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.*/
+
+let media;
+media = soma/numbers.length;
+console.log("A média é: " + media);
+
+// 4 -  Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"
+
+if(media > 20) {
+    console.log("valor maior que 20")
+}else {
+    console.log("valor menor ou igual a 20")
+}
+
+// 5 - Utilizando for, descubra qual o maior valor contido no array e imprima-o
+
+let maiorValor = numbers[0];
+
+for (var i in numbers){
+    if (numbers[i] >= maiorValor){
+        maiorValor = numbers[i];
+    }
+}
+console.log("O maior valor é: " + maiorValor);
+
+// 6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
