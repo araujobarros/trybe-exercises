5fa289cab8c39f6fa6b97800719d4765e4864732
Edson Araujo <araujo.barros@matematica.ufrj.br>
2020-10-27 15:16:31 -0300

    Começo atividade bonus: Lista e objetos

---
+++ bonus.js
@@ -0,0 +1,46 @@
+// Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+//Lista Input
+let listaRomana = ["C","M","X","I","V"];
+
+//Objeto de referência
+let romanosRef = {
+  um: {
+    valor: 1,
+    caracter:"I"
+  },
+  cinco: {
+    valor: 5,
+    caracter:"V",
+  },
+  dez: {
+    valor: 10,
+    caracter:"X",
+  },
+  cinquenta: {
+    valor: 50,
+    caracter:"L",
+  },
+  cem: {
+    valor: 100,
+    caracter:"C",
+  },
+  quinhentos: {
+    valor: 500,
+    caracter:"D",
+  },
+  mil: {
+    valor: 1000,
+    caracter:"M",
+  },
+}
+
+//variável auxiliar:
+let soma
+//Comparando o input com o objeto
+for(i in listaRomana){
+  for(chave in romanosRef){
+    if()
+  }
+}
+
