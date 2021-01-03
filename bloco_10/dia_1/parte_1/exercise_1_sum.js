51cbf4943ae1aed9566eaf1e5913ba458b1a6d6b
Edson Araujo <araujo.barros@matematica.ufrj.br>
2021-01-03 05:25:54 -0300

    Parte 1

---
+++ bloco_10/dia_1/parte_1/exercise_1_sum.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// // implemente seus testes aqui
+
+// assert.strictEqual(sum(4, 5), 9, '4 + 5 deve ser 9');
+
+// assert.strictEqual(sum(0, 0), 0, '0 + 0 deve ser 0');
+
+// console.log(sum(4,'5'));
+// // throw new Error('parameters must be numbers')
+
+// assert.strictEqual(sum(4, '5'), 9, 'Não utilize strings');
+// // throw new Error('parameters must be numbers')
+
+module.exports = sum;
